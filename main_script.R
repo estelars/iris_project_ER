0d400b172192fe70616129180aba82631b8e1eb9
estelars <estelaromerosotoca@gmail.com>
2024-02-07 10:40:38 +0100

    added plotting

--- main_script.R
+++ main_script.R
@@ -7,3 +7,7 @@ iris_data <- read_csv('raw_data/iris.csv')
 setosa_subset <- iris_data |> filter(Species == "setosa")
 # cool analysis
 model_setosa <- lm(Sepal.Length ~ Petal.Length, data = setosa_subset)
+# plotting
+setosa_plot <- ggplot(setosa_subset, aes(x = Petal.Length, y = Sepal.Length)) +
+  geom_point(aes(size = Petal.Width)) +
+  stat_smooth
